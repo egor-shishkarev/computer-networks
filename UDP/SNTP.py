b410be774a336a1451c680726f5148fa9164bb67
egor-shishkarev <egorsiskarev92@gmail.com>
2024-11-08 15:35:11 +0300

    feat: add SNTP request

---
+++ UDP/SNTP.py
@@ -0,0 +1,39 @@
+import socket
+import struct
+import time
+import select
+
+NTP_SERVER0 = "0.ru.pool.ntp.org"
+NTP_SERVER1 = "1.ru.pool.ntp.org"
+NTP_SERVER2 = "2.ru.pool.ntp.org"
+NTP_SERVER3 = "3.ru.pool.ntp.org"
+PORT = 123
+TIME1970 = 2208988800
+
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+data = '\x1b' + 47 * '\0'
+
+try:
+    client.sendto( data.encode('utf-8'), (NTP_SERVER0, PORT))
+    client.sendto( data.encode('utf-8'), (NTP_SERVER1, PORT))
+    client.sendto( data.encode('utf-8'), (NTP_SERVER2, PORT))
+    client.sendto( data.encode('utf-8'), (NTP_SERVER3, PORT))
+    rdy = select.select([client], [], [], 0.9)
+
+    for i in range(4):
+        rdy = select.select([client], [], [], 0.9)
+
+        if not rdy[0]:
+            raise RuntimeError("socket recv broken")
+        
+        data, address = client.recvfrom(1024)
+        
+        if data:
+            print ('Response received from:', address)
+            t = struct.unpack( '!12I', data)[8]
+            t -= TIME1970
+            print ('\tTime=%s' % time.ctime(t))
+
+except Exception as e:
+    print("Socket error: " + str(e))
